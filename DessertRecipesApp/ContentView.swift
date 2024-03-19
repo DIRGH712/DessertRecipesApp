c96fcc629653d991e72b498cdbfa18fe7f421494
dppate13 <dppate13@asu.edu>
2024-03-18 17:08:05 -0700

    Initial Commit

---
+++ DessertRecipesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DessertRecipesApp
+//
+//  Created by Dirgh Patel on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DessertRecipesApp/DessertRecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesAppApp.swift
+//  DessertRecipesApp
+//
+//  Created by Dirgh Patel on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
